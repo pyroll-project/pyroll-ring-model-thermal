545d4a9ee56bd78944018dbc29f2b83a7f7926a8
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-01-13 09:55:08 +0100

    sort symbols

--- docs/task_latex.py
+++ docs/task_latex.py
@@ -1,9 +1,12 @@
 import pytask
 
+from docs.config import BUILD_DIR
+
 
 @pytask.mark.latex(
     script="docs.tex",
     document="docs.pdf",
 )
+@pytask.mark.depends_on(BUILD_DIR.rglob("*"))
 def task_latex():
     pass
--- docs/task_process_jinja.py
+++ docs/task_process_jinja.py
@@ -3,22 +3,22 @@ import pytask
 from pathlib import Path
 import toml
 
-THIS_DIR = Path(__file__).parent
+from docs.config import BUILD_DIR, ROOT_DIR
 
-JINJA_ENV = jinja2.Environment(loader=jinja2.FileSystemLoader(THIS_DIR, encoding="utf-8"))
+JINJA_ENV = jinja2.Environment(loader=jinja2.FileSystemLoader(ROOT_DIR, encoding="utf-8"))
 
 
 def jinja_task(tex_file: Path | str):
-    tex_file = (Path(THIS_DIR / tex_file) if isinstance(tex_file, str) else tex_file).resolve()
+    tex_file = (Path(ROOT_DIR / tex_file) if isinstance(tex_file, str) else tex_file).resolve()
 
     def dec(func):
-        @pytask.mark.task(id=str(tex_file.relative_to(THIS_DIR)))
+        @pytask.mark.task(id=str(tex_file.relative_to(ROOT_DIR)))
         @pytask.mark.depends_on({
             "template": tex_file,
         })
-        @pytask.mark.produces(THIS_DIR / ".build" / tex_file.relative_to(THIS_DIR))
+        @pytask.mark.produces(BUILD_DIR / tex_file.relative_to(ROOT_DIR))
         def task_process_jinja(depends_on: dict[str, Path], produces: Path):
-            template = JINJA_ENV.get_template(str(depends_on["template"].relative_to(THIS_DIR)))
+            template = JINJA_ENV.get_template(str(depends_on["template"].relative_to(ROOT_DIR)))
 
             result = template.render(func(depends_on=depends_on))
 
@@ -35,6 +35,11 @@ def jinja_task(tex_file: Path | str):
 @jinja_task("symbol_index.tex")
 def symbol_index(depends_on: dict[str, Path]):
     symbols = toml.loads(depends_on["data"].read_text())["symbol"]
+
+    def get_sort_key(e):
+        return (e.get("sort", None) or e["code"]).removeprefix("\\Delta").strip("\\ ").lower()
+
+    symbols = sorted(symbols, key=get_sort_key)
     return dict(
         symbols=symbols
     )

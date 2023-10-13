4da48c67cfcef8b86709bfd3cc4fe4f9b513460a
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-10-13 09:25:03 +0200

    add profile plot

--- tests/test_inhomogeneous_profile.py
+++ tests/test_inhomogeneous_profile.py
@@ -11,34 +11,40 @@ from pyroll.ring_model import RingProfile
 
 @pytest.mark.parametrize(
     "p", [
-        Profile.round(radius=10, core_temperature=1173.15, surface_temperature=1273.15),
-        Profile.square(side=10, corner_radius=1, core_temperature=1173.15, surface_temperature=1273.15),
-        Profile.box(height=10, width=5, corner_radius=1, core_temperature=1173.15, surface_temperature=1273.15),
-        Profile.diamond(height=5, width=10, corner_radius=1, core_temperature=1173.15, surface_temperature=1273.15)
+        Profile.round(radius=10, core_temperature=973.15, surface_temperature=1273.15),
+        Profile.square(side=10, corner_radius=1, core_temperature=973.15, surface_temperature=1273.15),
+        Profile.box(height=10, width=5, corner_radius=1, core_temperature=973.15, surface_temperature=1273.15),
+        Profile.diamond(height=5, width=10, corner_radius=1, core_temperature=973.15, surface_temperature=1273.15),
+        Profile.round(radius=10, core_temperature=1273.15, surface_temperature=973.15),
+        Profile.square(side=10, corner_radius=1, core_temperature=1273.15, surface_temperature=973.15),
+        Profile.box(height=10, width=5, corner_radius=1, core_temperature=1273.15, surface_temperature=973.15),
+        Profile.diamond(height=5, width=10, corner_radius=1, core_temperature=1273.15, surface_temperature=973.15)
     ]
 )
 def test_inhomogeneous_in_profile(p: Union[RingProfile, Profile]):
     try:
         pyroll.ring_model.Config.RING_COUNT = 15
         fig: plt.Figure = plt.figure()
-        ax: plt.Axes = fig.subplots()
-        ax.set_aspect("equal")
+        axs: list[plt.Axes] = fig.subplots(nrows=2)
+        axs[0].set_aspect("equal")
         cmap = plt.get_cmap('viridis')
         norm = Normalize(vmin=min(p.ring_temperatures), vmax=max(p.ring_temperatures))
 
         for ring, temperature in zip(reversed(p.ring_sections), reversed(p.ring_temperatures)):
             lines = ring.boundary
             if isinstance(lines, shapely.MultiLineString):
-                ax.fill(*lines.geoms[0].xy, color=cmap(norm(temperature)))
-                ax.fill(*lines.geoms[1].xy, color='black')
+                axs[0].fill(*lines.geoms[0].xy, color=cmap(norm(temperature)))
+                axs[0].fill(*lines.geoms[1].xy, color='black')
             else:
-                ax.fill(*lines.xy, alpha=0.5, color=cmap(norm(temperature)))
+                axs[0].fill(*lines.xy, alpha=0.5, color=cmap(norm(temperature)))
 
         sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
         sm.set_array([])
-        cbar = plt.colorbar(sm, ax=ax, label='Temperature')
+        cbar = plt.colorbar(sm, ax=axs[0], label='Temperature')
 
-        ax.plot(*p.cross_section.boundary.xy, c="black")
+        axs[0].plot(*p.cross_section.boundary.xy, c="black")
+
+        axs[1].plot(p.rings, p.ring_temperatures)
 
         fig.tight_layout()
         plt.show()
